a83db79d8a00e5cdfb1bcd716f0289b186251d19
ajaykumar-bollepalli <ajay.bollepalli@gmail.com>
2017-08-02 14:35:39 +0530

    starting index page is been created

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head lang="en-in">
+    <title>Welcome to Lesson Plan</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"> 
+</head>
+<body class="w3-container">
+    <header class="w3-container w3-teal">
+        Heading goes here
+    </header>
+
+    <?
+    
+    ?>
+</body>
+</html>
